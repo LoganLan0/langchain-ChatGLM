91c89ad0376f507ab160abf71178157628b1ab98
LoganLan 兰浩 <122266047+LoganLan0@users.noreply.github.com>
2023-08-03 13:42:04 +0800

    Update model_config.py

--- configs/model_config.py
+++ configs/model_config.py
@@ -25,7 +25,7 @@ embedding_model_dict = {
 }
 
 # Embedding model name
-EMBEDDING_MODEL = "text2vec"
+EMBEDDING_MODEL = "text2vec-base-chinese-paraphrase"
 
 # Embedding running device
 EMBEDDING_DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
